0a8e760a8bc9b3ecf616c2fa30b5c62ceca72cdf
GSU <s.gladyshev@notamedia.ru>
2020-12-22 04:16:25 +0900

    поменял дизайн попапа целиком

--- src/background.js
+++ src/background.js
@@ -2,8 +2,10 @@ chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
     if (msg === 'rerun') {
       chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "rerun"}, function (response) {
-        });
+        if (tabs[0]) {
+          chrome.tabs.sendMessage(tabs[0].id, {action: "rerun"}, function (response) {
+          });
+        }
       });
     }
   });
--- src/popup.js
+++ src/popup.js
@@ -11,7 +11,7 @@ runButton[0].addEventListener("click", function () {
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {action: "hasTranslated"}, function (response) {
-        if (response.hasTranslated) {
+        if (response && response.hasTranslated) {
             runContainer[0].innerHTML = '<h5 class="text-white op-7">Translation completed</h5>';
         }
     });
