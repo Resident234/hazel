ce23ef3caa0e9e7519f668cf8c961ba53ea112e1
GSU <s.gladyshev@notamedia.ru>
2020-12-22 14:35:06 +0900

    поправил работу замены кнопки текстом

--- src/popup.js
+++ src/popup.js
@@ -12,7 +12,7 @@ runButton[0].addEventListener("click", function () {
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {action: "hasTranslated"}, function (response) {
         if (response && response.hasTranslated) {
-            runContainer[0].innerHTML = '<h5 class="text-white op-7">Translation completed</h5>';
+            runContainer[0].innerHTML = 'Translation completed';
         }
     });
 });
