82e7b8a46f1b656a51381491648a49e5c2696157
GSU <s.gladyshev@notamedia.ru>
2021-01-15 04:14:27 +0900

    логика показа и сокрытия попапа для предложения

--- src/components/hover.js
+++ src/components/hover.js
@@ -1,7 +1,33 @@
 export function initHover() {
-    var element = document.getElementsByTagName("hover")[0];
-    element.onmouseover = function (event) {
-        event.target.insertAdjacentHTML('afterbegin', '<hover-popup>====</hover-popup>');
-        console.log('onmouseover');
+    const elements = document.getElementsByTagName("hover");
+    for (let element of elements) {
+        element.onmouseover = function (event) {
+            if (event.target.tagName.toLowerCase() === 'hover') {
+                if (event.target.classList.contains('js-hazel--is-hover-popup-created')) {
+                    event.target.firstChild.style.display = 'block';
+                } else {
+                    event.target.style.position = 'relative';
+                    event.target.classList.add('js-hazel--is-hover-popup-created');
+
+                    let hoverPopup = document.createElement('hover-popup');
+                    let compStyles = window.getComputedStyle(event.target);
+                    hoverPopup.innerText = event.target.innerText;
+                    hoverPopup.style.width = '1000px';//TODO: вычислить ширину пока не получилось
+                    hoverPopup.style.height = compStyles.lineHeight;
+                    hoverPopup.style.top = '-' + compStyles.lineHeight;
+                    hoverPopup.style.position = 'absolute';
+                    hoverPopup.style.backgroundColor = compStyles.backgroundColor;
+
+                    event.target.insertAdjacentHTML(
+                        'afterbegin',
+                        hoverPopup.outerHTML
+                    );
+                }
+            }
+
+        };
+        element.onmouseout = function (event) {
+            event.target.firstChild.style.display = 'none';
+        };
     }
 }
