eee0f90cb90db83c40204c325c8d032b1b076aa7
Gladyshev Sergey <gsu1234@mail.ru>
2022-05-02 20:12:17 +0300

    Объединение css файлов

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
 const {resolve} = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = env => {
   return {
     entry: {
-      'translator': './src/app.js',
-      'options': './src/app/options/options.js',
-      'popup': './src/app/popup/popup.js',
+      'translator.js': './src/app.js',
+      'options.js': './src/app/options/options.js',
+      'popup.js': './src/app/popup/popup.js',
+      'style.css': [
+        './src/app/style.css',
+        './src/components/delimiter/style.css',
+        './src/components/initiation/style.css',
+        './src/components/spinner/style.css'
+      ]
     },
     output: {
-      filename: '[name].js',
+      filename: '[name]',
       path: resolve(__dirname, 'dist'),
       pathinfo: !env.prod,
     },
     devtool: env.prod ? 'source-map' : 'eval',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin("style.css"),
+    ]
   };
 };
