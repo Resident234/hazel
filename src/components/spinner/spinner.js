544aff0493fce38860f121827b74732ebd94fe36
Gladyshev Sergey <gsu1234@mail.ru>
2022-05-07 17:02:11 +0300

    Поправил стиль спиннера

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,7 +3,7 @@ const spinner = () => {
   spinner.className = 'js-translator-spinner translator-spinner js-translator-component-element'
   spinner.innerHTML = '<div id="uvc-type-wrap-1980"  ' +
     'data-ultimate-target=\'#uvc-type-wrap-1980\'  ' +
-    'data-responsive-json-new=\'{"font-size":"desktop:45px;","line-height":"desktop:50px;"}\'  ' +
+    'data-responsive-json-new=\'{"font-size":"desktop:45px;","line-height":"desktop:56px;"}\'  ' +
     'class="js-translator-component-element uvc-type-wrap ult-adjust-bottom-margin ult-responsive  uvc-type-align-left uvc-type-no-prefix uvc-wrap-31290215265fdf3912d95f8" ' +
     'style="text-align:left;">' +
     '<span id="typed-31290215265fdf3912d95f8" class="ultimate-typed-main js-translator-component-element" ' +
